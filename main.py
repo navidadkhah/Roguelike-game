10bda7d73d0ae7650e85f9b0dbe8d539d330a455
navid dadkhah <nnd1380@gmail.com>
2025-01-04 13:43:48 +0330

    adding arts

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ class Camera:
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load("images/main_character.png")
-        self.image = pygame.transform.scale(self.image, (TILE_SIZE - 10, TILE_SIZE - 10))
+        self.image = pygame.image.load("images/Knight/knight1.png")
+        self.image = pygame.transform.scale(self.image, (TILE_SIZE , TILE_SIZE ))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.dx, self.dy = 0, 0
